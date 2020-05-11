32e70d1cce2aa0ea60eeb997db1028c55b2195cb
Igor Dias <igordiasth@gmail.com>
2020-05-11 17:42:59 +1200

    show errors in input

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,10 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -11,21 +14,26 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
-        name: Yup.string().required('Name is required'),
+        name: Yup.string().required('Name is required.'),
         email: Yup.string()
-          .required('E-mail is required')
+          .required('E-mail is required.')
           .email('Enter a valid e-mail.'),
-        password: Yup.string().min(6, 'Enter the min 6 digits'),
+        password: Yup.string().min(6, 'Enter at least 6 digits.'),
       });
 
       await schema.validate(data, {
         abortEarly: false,
       });
     } catch (err) {
-      console.log(err);
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
     }
   }, []);
   return (
@@ -34,7 +42,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="Razor Mustache" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Sign Up</h1>
           <Input name="name" icon={FiUser} placeholder="Name" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
