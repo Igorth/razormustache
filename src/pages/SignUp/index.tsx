12f124d1760773603a0d0a13b4f5b3b4a1747a34
Igor Dias <igordiasth@gmail.com>
2020-05-14 16:41:25 +1200

    starting configure routes

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,6 +3,7 @@ import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 
@@ -11,7 +12,7 @@ import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, Background, AnimationContainer } from './styles';
 
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
@@ -40,26 +41,28 @@ const SignUp: React.FC = () => {
     <Container>
       <Background />
       <Content>
-        <img src={logoImg} alt="Razor Mustache" />
+        <AnimationContainer>
+          <img src={logoImg} alt="Razor Mustache" />
 
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Sign Up</h1>
-          <Input name="name" icon={FiUser} placeholder="Name" />
-          <Input name="email" icon={FiMail} placeholder="E-mail" />
-          <Input
-            name="password"
-            icon={FiLock}
-            type="password"
-            placeholder="Password"
-          />
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Sign Up</h1>
+            <Input name="name" icon={FiUser} placeholder="Name" />
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="Password"
+            />
 
-          <Button type="submit">Sign Up</Button>
-        </Form>
+            <Button type="submit">Sign Up</Button>
+          </Form>
 
-        <a href="home">
-          <FiArrowLeft />
-          Back home
-        </a>
+          <Link to="/">
+            <FiArrowLeft />
+            Back home
+          </Link>
+        </AnimationContainer>
       </Content>
     </Container>
   );
--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -3,6 +3,7 @@ import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
 
 import { useAuth } from '../../hooks/auth';
 import { useToast } from '../../hooks/toast';
@@ -14,7 +15,7 @@ import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Content, Background } from './styles';
+import { Container, Content, Background, AnimationContainer } from './styles';
 
 interface SignInFormData {
   email: string;
@@ -50,7 +51,10 @@ const SignIn: React.FC = () => {
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err);
+
           formRef.current?.setErrors(errors);
+
+          return;
         }
 
         addToast({
@@ -65,29 +69,31 @@ const SignIn: React.FC = () => {
   return (
     <Container>
       <Content>
-        <img src={logoImg} alt="Razor Mustache" />
+        <AnimationContainer>
+          <img src={logoImg} alt="Razor Mustache" />
 
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Sign In</h1>
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Sign In</h1>
 
-          <Input name="email" icon={FiMail} placeholder="E-mail" />
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-          <Input
-            name="password"
-            icon={FiLock}
-            type="password"
-            placeholder="password"
-          />
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="password"
+            />
 
-          <Button type="submit">Enter</Button>
+            <Button type="submit">Enter</Button>
 
-          <a href="forgot">Forgot my password</a>
-        </Form>
+            <a href="forgot">Forgot my password</a>
+          </Form>
 
-        <a href="login">
-          <FiLogIn />
-          Create account
-        </a>
+          <Link to="/signup">
+            <FiLogIn />
+            Create account
+          </Link>
+        </AnimationContainer>
       </Content>
       <Background />
     </Container>
